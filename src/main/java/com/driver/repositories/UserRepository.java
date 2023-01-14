ac1bbc1ca3ea49c013f97b9265a5b24339029eeb
ajaya <vodlaajay234@gmail.com>
2023-01-14 21:24:36 +0530

    firstCommit

--- src/main/java/com/driver/repositories/UserRepository.java
+++ src/main/java/com/driver/repositories/UserRepository.java
@@ -1,6 +1,6 @@
 package com.driver.repositories;
 
-import com.driver.models.Blog;
+
 import com.driver.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -11,5 +11,9 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends CrudRepository<User,Integer> {
+
+
+
+    User findByUsername(String username);
 }
--- src/main/java/com/driver/services/UserService.java
+++ src/main/java/com/driver/services/UserService.java
@@ -16,17 +16,32 @@ public class UserService {
     @Autowired
     BlogService blogService3;
 
-    public void createUser(User user){
-    }
+    public void createUser(User user) {
 
-    public void deleteUser(int userId){
-    }
-
-    public void updateUser(User user){
         userRepository3.save(user);
     }
 
-    public User findUserByUsername(String username){
+    public void deleteUser(int userId) {
+        if (userRepository3.findById(userId).isPresent()) {
+            userRepository3.deleteById(userId);
+        }
+    }
+
+    public void updateUser(User user) {
+        User currentUser = userRepository3.findByUsername(user.getUsername());
+        if (currentUser != null) {
+            currentUser.setUsername(user.getUsername());
+            currentUser.setFirstName(user.getFirstName());
+            currentUser.setLastName(user.getLastName());
+            currentUser.setPassword(user.getPassword());
+
+            userRepository3.save(currentUser);
+        }
+        userRepository3.save(user);
+
+    }
+
+    public User findUserByUsername(String username) {
         return userRepository3.findByUsername(username);
     }
 }
